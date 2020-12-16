20d5e0397190840b3b63743232a50708ac4ad04f
Arsh Noor Amin <ana002@bucknell.edu>
2020-12-16 12:06:37 -0600

    contact page init

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,17 @@
 let contactHTML = `
-  <h1>I am contact Page</h1>
+<div class="row mx-xl-5">
+  <div id="contact" class="col m-auto">
+      <p class="black-font-color">My <b>Social</b> 
+        <a class="black-font-color ml-2" href="https://www.facebook.com/arshnoor.lakhani/" target="_blank"> <i class="fab fa-facebook"></i> </a>
+        <a class="black-font-color ml-2" href="https://www.facebook.com/arshnoor.lakhani/" target="_blank"> <i class="fab fa-instagram"></i> </a>
+      </p>
+      <p class="black-font-color">Keep up with My <b>Work</b> at 
+        <a class="black-font-color ml-2" href="https://www.facebook.com/arshnoor.lakhani/" target="_blank"> <i class="fab fa-linkedin"></i> </a>
+        <a class="black-font-color ml-2" href="https://www.facebook.com/arshnoor.lakhani/" target="_blank"> <i class="fab fa-github"></i> </a>
+        <a class="black-font-color ml-2" href="https://www.facebook.com/arshnoor.lakhani/" target="_blank"> <i class="fab fa-behance"></i> </a>
+      </p>
+      <p class="black-font-color">Or <b>Reach Me</b> at</p>
+      <p id="email" class="black-font-color">arshnoor.amin@bucknell(dot)edu</p>
+  </div>
+</div>
 `
